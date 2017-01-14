3c4b6237d8dc5e6b2e0e1f4ff13b8f8eb49ba5cd
Shubham yeole <shubham20.yeole@gmail.com>
2017-01-14 12:47:24 -0500

    Add VisitorMap Project

---
+++ app.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var SparkPost = require('sparkpost');
+var sp = new SparkPost('9bf6b6d7079252cab943971ff90c08cc3a9cee0d');
+var port = process.env.PORT || 3000
+var bodyParser = require('body-parser');
+var path = require('path');
+var expressValidator = require('express-validator');
+var mongojs = require('mongojs')
+var mongodb = require('mongodb')
+// var db = mongojs('mongodb://ds143717.mlab.com:43717/shubham', ['users']);
+var collections = ["users", "blog", "comments", "property", "images", "notification", "bookmark", "messages","timetable", "timetablecategory", "timetablequestion", "resume", "skills", "locations"]
+
+var db = mongojs('mongodb://shubham20.yeole:shubham20.yeole@ds163387.mlab.com:63387/paceteam3', collections)
+
+var app = express();
+var ObjectId = mongojs.ObjectId;
+var passport = require("passport")
+
+
+
+// View Engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname)));
+// body parser middleware
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb',extended: false}));
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname)));
+
+
+// This method is ADMIN SIDE METHOD
+app.get('/', function(req, res){  
+var pageno = Number(0);  
+  db.locations.find(function (err, locs) {
+      res.render("visitormap.ejs",{locs: locs});
+  })
+});
+app.post('/searchLocation', function(req, res) {
+  var id = req.body.id;
+  var ObjectID = require('mongodb').ObjectID;
+  var o_id = new ObjectID(id);
+   db.locations.findOne({ '_id': o_id}, function (err, location) {
+    res.send(location);
+  });
+});
+app.listen(port, function() {
+  console.log('Listening on port ' + port)
+})
+
+
